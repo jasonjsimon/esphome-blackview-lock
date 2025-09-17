e1015cc2876f973016a1ae376aca3ee0720b37bc
jasonjsimon <103650963+jasonjsimon@users.noreply.github.com>
2025-09-17 13:34:36 -0500

    Update __init__.py

--- components/blackview_lock/__init__.py
+++ components/blackview_lock/__init__.py
@@ -1,24 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import esp32_ble_client
-from esphome.const import CONF_ID, CONF_BLE_CLIENT_ID
+# This import has been corrected
+from esphome.const import CONF_ID
 
-# This explicitly declares the component's dependency.
 DEPENDENCIES = ['esp32_ble_client']
 
 blackview_lock_ns = cg.esphome_ns.namespace('blackview_lock')
 BlackviewLock = blackview_lock_ns.class_('BlackviewLock', cg.Component, esp32_ble_client.BLEClientBase)
 
-# This is the modern, direct way to define the schema without outdated helpers.
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BlackviewLock),
-    cv.Required(CONF_BLE_CLIENT_ID): cv.use_id(esp32_ble_client.ESP32BLEClient),
+    # The constant has been replaced with the actual string
+    cv.Required("ble_client_id"): cv.use_id(esp32_ble_client.ESP32BLEClient),
 }).extend(cv.COMPONENT_SCHEMA)
 
-# This is the modern way to register the component with its BLE client parent.
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    parent = await cg.get_variable(config[CONF_BLE_CLIENT_ID])
+    # The constant has been replaced with the actual string
+    parent = await cg.get_variable(config["ble_client_id"])
     cg.add(parent.register_node(var))
