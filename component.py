a8142e0195907fe31b84f66341b6670f0c71db1f
jasonjsimon <103650963+jasonjsimon@users.noreply.github.com>
2025-09-17 13:10:25 -0500

    Delete component.py

--- component.py
+++
@@ -1,8 +0,0 @@
-import esphome.config_validation as cv
-
-# The simplest possible schema to prove the file is being loaded.
-CONFIG_SCHEMA = cv.Schema({})
-
-async def to_code(config):
-    # This function does nothing for the test.
-    pass
