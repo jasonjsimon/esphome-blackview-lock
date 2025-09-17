63a2de936223fb890faf8694844b19c3172e49dd
jasonjsimon <103650963+jasonjsimon@users.noreply.github.com>
2025-09-17 13:37:48 -0500

    Update __init__.py

--- components/blackview_lock/__init__.py
+++ components/blackview_lock/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import esp32_ble_client
-# This import has been corrected
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['esp32_ble_client']
@@ -11,14 +10,13 @@ BlackviewLock = blackview_lock_ns.class_('BlackviewLock', cg.Component, esp32_bl
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BlackviewLock),
-    # The constant has been replaced with the actual string
-    cv.Required("ble_client_id"): cv.use_id(esp32_ble_client.ESP32BLEClient),
+    # This now uses the class name suggested by a previous error message.
+    cv.Required("ble_client_id"): cv.use_id(esp32_ble_client.BLEClientBase),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    # The constant has been replaced with the actual string
     parent = await cg.get_variable(config["ble_client_id"])
     cg.add(parent.register_node(var))
