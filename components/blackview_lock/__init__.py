0f5f34721a7fdc9938d9ccaae4de60bd19a7a2a8
jasonjsimon <103650963+jasonjsimon@users.noreply.github.com>
2025-09-17 13:29:11 -0500

    Update __init__.py

--- components/blackview_lock/__init__.py
+++ components/blackview_lock/__init__.py
@@ -4,12 +4,12 @@ from esphome.components import esp32_ble_client
 from esphome.const import CONF_ID
 
 blackview_lock_ns = cg.esphome_ns.namespace('blackview_lock')
-BlackviewLock = blackview_lock_ns.class_('BlackviewLock', cg.Component, esp32_ble_client.BLEClientNode)
+# The only change is BLEClientNode -> BLEClientBase in the line below
+BlackviewLock = blackview_lock_ns.class_('BlackviewLock', cg.Component, esp32_ble_client.BLEClientBase)
 
-# This schema is what defines the valid options for your component in YAML
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BlackviewLock),
-}).extend(esp32_ble_client.BLE_CLIENT_SCHEMA) # This line adds the "ble_client_id" option
+}).extend(esp32_ble_client.BLE_CLIENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
